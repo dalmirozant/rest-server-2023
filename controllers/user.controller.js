af0059b07a74f76b914247402f983705a32cb2cf
Dalmiro <dalmirozant@gmail.com>
2023-04-09 19:10:45 -0300

    Parámetros y querys

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,23 +3,30 @@ const {response} = require('express');
 
 
 const getUsers = (req, res = response) => {
+   const {id,page=10} = req.query;
     res.json({
         ok: true,
-        msg:'Corriendo API Controller'
+        msg:'Corriendo API Controller',
+        id,
+        page,
     });
   }
 
 const updateUser = (req, res) => {
+    const {id} = req.params;
     res.json({
         ok: true,
-        msg:'put API'
+        msg:'put API',
+        id,
     });
   }
 
 const createUser = (req, res) => {
+    const body = req.body;
     res.json({
         ok: true,
-        msg:'post API'
+        msg:'post API',
+        body,
     });
   }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/', getUsers);
   
-router.put('/', updateUser);
+router.put('/:id', updateUser);
   
 router.post('/', createUser);
   
