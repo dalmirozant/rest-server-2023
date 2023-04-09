f647de2b2585fbbd2911f795f66f806b6a52488d
Dalmiro <dalmirozant@gmail.com>
2023-04-09 15:17:00 -0300

    express funcional

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("HOLA MUNDO")
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`La aplicación está corriendo en el puerto ${process.env.PORT}`);
+  });
